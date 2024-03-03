24c1b336519563cce2252811e24ce83270832e8f
Alexandr <User@ALEXANDR>
2024-03-03 19:54:31 +0800

    checkpoint

--- MB_Diagnostics_App/Views/Windows/MultimetrWindow.xaml.cs
+++ MB_Diagnostics_App/Views/Windows/MultimetrWindow.xaml.cs
@@ -10,24 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace experimenthal
+namespace MB_Diagnostics_App.Views.Windows
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for MultimetrWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MultimetrWindow : Window
     {
-        public MainWindow()
+        public MultimetrWindow()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
