9f6ff11848c9a48bf5a9e46480ba15e327e287d5
Alexandr <User@ALEXANDR>
2024-03-09 21:19:56 +0800

    add menu and change photo

--- MB_Diagnostics_App/Views/Windows/MainWindow.xaml.cs
+++ MB_Diagnostics_App/Views/Windows/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace MB_Diagnostics_App
         {
             InitializeComponent();
         }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
     }
 }
